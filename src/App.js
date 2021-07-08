a2e22436cfe5feb0fe0c860fba673c6971fe169b
jassem-lab <jassemgaaloul123@gmail.com>
2021-07-08 01:11:07 +0100

    testing section two component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,10 @@
 import "./App.scss";
+import Services from "./components/services";
 
 function App() {
   return (
     <>
-  
-      <section>
-        <div class="wave">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
-
-        <div className="container">
-            <div className="row footer__title">LATEN WE IETS GEWELDIGS CREËREN</div>
-            <div className="row info">
-            <div className="footer__email">
-              <h3>E-mail:</h3>
-              <a href="mailto:info@laforga.nl">info@laforga.nl</a>
-            </div>
-            <div>
-            <h3>Telefoonnummer</h3> <a href="tel:+31238440060">023 8440060</a>
-            </div>
-            <div className="social-links">
-            <h3>Social</h3>
-            <div>
-              {" "}
-              <a href="">
-                Instagram{" "}
-                <span>
-                  {" "}
-                  {/* <img src={Vector} alt="" /> */}
-                </span>
-              </a>
-              <a href="">
-                Facebook{" "}
-                <span>
-                  {" "}
-                  {/* <img src={Vector} alt="" /> */}
-                </span>
-              </a>
-              <a href="">
-                LinkedIn{" "}
-                <span>
-                  {" "}
-                  {/* <img src={Vector} alt="" /> */}
-                </span>
-              </a>
-              <a href="">
-                TikTok{" "}
-                <span>
-                  {" "}
-                  {/* <img src={Vector} alt="" /> */}
-                </span>
-              </a>
-              <a href="">
-                Whatsapp{" "}
-                <span>
-                  {" "}
-                  {/* <img src={Vector} alt="" /> */}
-                </span>
-              </a>
-            </div>
-          </div>
-            </div>
-         </div>
-
-      </section>
+       Load component ... 
     </>
   );
 }
---
+++ src/components/services.js
@@ -0,0 +1,121 @@
+import React from "react";
+import "../App.scss";
+const Services = () => {
+  return (
+    <div>
+      <div className="main-section-six" id="doen-wij">
+        <div className="services-container">
+          <div className="services-intro">
+            <div className="services-intro-text">
+              <h1>Wat doen wij?</h1>
+              <p>
+                Wij helpen u in alle fasen van de realisatie van uw digitale
+                project.
+              </p>
+            </div>
+
+            <div className="detail-services-container">
+              <div className="detail-services-elm">
+                <img
+                  src=""
+                  alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+                />
+                <p>Strategie</p>
+              </div>
+              <div className="detail-services-elm">
+                <img
+                  src=""
+                  alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+                />
+                <p>Ontwerp & branding</p>
+              </div>
+              <div className="detail-services-elm">
+                <img
+                  src=""
+                  alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+                />
+                <p>Webontwikkeling</p>
+              </div>
+              <div className="detail-services-elm">
+                <img
+                  src=""
+                  alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+                />
+                <p>Support & groei</p>
+              </div>
+            </div>
+          </div>
+          <div className="services-images">
+            <img
+              src=""
+              alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+            />
+            <img
+              src=""
+              alt="shopify partners webshop ecommerce agency we design build bouwen mooie webshop modern fris website online verkopen"
+            />
+          </div>
+          <div className="services">
+          <div className={`services-strategy service`}>
+            <h2>
+              Strategie<span id="number">1</span>
+            </h2>
+            <p>
+              Alleen uw succes is ons succes. Wij leven ons in & verplaatsen ons
+              volledig in uw(of de klant) en proberen vanuit dit perspectief mee
+              te denken. We brengen quality time door met onze klanten om te
+              ontdekken waar hun merk echt voor staat & ons te verdiepen in het
+              bedrijf. Onze UX-designers analyseren de concurrentie en kijken
+              hoe wij hun voor kunnen zijn.
+            </p>
+          </div>
+          <div className={`services-design service`}>
+            <h2>
+              Design<span id="number">2</span>
+            </h2>
+            <p>
+              Wij geloven dat het ontwerp van een website de belangrijkste
+              factor is bij het omzetten van bezoekers in klanten. Met een jong,
+              fris & ambitieus team zijn wij op de hoogte van de laatste trends
+              & ontwerpen wij de meest moderne websites & webshops. Met
+              innovatieve ontwerpoplossingen zorgen we ervoor dat uw winkel uw
+              klanten aanspreekt. We combineren design en digitale innovatie om
+              emotie en verhalen te creëren.
+            </p>
+          </div>
+          <div className={`services-development service`}>
+            <h2>
+              Webontwikkeling <span id="number">3</span>
+            </h2>
+            <p>
+              Onze focus ligt altijd op de bruikbaarheid,
+              gebruiksvriendelijkheid en de prestaties van de winkels. Ons team
+              brengt designs tot leven. Snel en geoptimaliseerd voor maximale
+              conversie. We ontwikkelen voor het nu en bouwen voor de toekomst.
+              . Met behulp van moderne technologieën bouwen we flexibele en
+              schaalbare oplossingen.
+            </p>
+          </div>
+
+          <div className={`services-support service`}>
+            <h2>
+              Optimalisering & support<span id="number">4</span>
+            </h2>
+            <p>
+              Heb je al een{" "}
+              <span className="bold">bestaande Shopify store</span> en voel je
+              je beperkt door je thema’s? Dan ben je bij ons op het juiste
+              adres. Wij maken custom pagina’s en onderdelen voor jouw webshop.
+              Voel je nooit beperkt! Wij zijn in staat om jouw digitale wensen
+              tot vervulling laten komen. Neem contact met ons op dan kunnen we
+              kijken wat wij voor je kunnen betekenen!
+            </p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
